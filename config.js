81d936f44b914a6033f4f8e104aeffafc3fd9b82
PotapIlya <ilya.burenko.01@mail.ru>
2021-04-29 11:54:55 +0300

    12

---
+++ config.js
@@ -0,0 +1,55 @@
+module.exports = {
+
+    'basic' : {
+        'token' : '1702336014:AAHuZM2dF8vi7ABxTNA6sxFN_F2TDOkbdqY',
+    },
+
+    'keyboard' : [
+        [
+            {
+                text: 'Btn_1', // текст на кнопке
+            },
+            {
+                text: 'Btn_2',
+            }
+        ],
+        [
+            {
+                text: 'Btn_3',
+            }
+        ]
+    ],
+
+    'keyboard_1' : [
+        [
+            {
+                text: '1'
+            },
+            {
+                text: '1'
+            },
+        ],
+    ],
+    'keyboard_2' : [
+        [
+            {
+                text: '2'
+            },
+            {
+                text: '2'
+            },
+        ],
+    ],
+    'keyboard_3' : [
+        [
+            {
+                text: '3'
+            },
+            {
+                text: '3'
+            },
+        ],
+    ],
+
+
+};
